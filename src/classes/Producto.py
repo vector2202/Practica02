c1ce8a924be32f15dd365084d18796bbf49bd4cc
ulisess_ap <ulisessap12@ciencias.unam.mx>
2023-03-05 23:39:46 -0600

    clases empleado,producto,interfaz

--- src/classes/Producto.py
+++ src/classes/Producto.py
@@ -1,8 +1,9 @@
-import Interfaz
+import src.classes.Interfaz as Interfaz
 
-class Producto(Interfaz.Interfaz):
 
-    def __init__(self,identificador, nombre, fechaPreparacion, precio,cantidad, marca, presentacion, refrigeracion, fechaCaducidad):
+class Producto(Interfaz):
+
+    def __init__(self, identificador, nombre, fechaPreparacion, precio, cantidad, marca, presentacion, refrigeracion, fechaCaducidad):
         super().__init__(identificador, nombre, fechaPreparacion)
         self.precio = precio
         self.cantidad = cantidad
@@ -10,3 +11,30 @@ class Producto(Interfaz.Interfaz):
         self.presentacion = presentacion
         self.refrigeracion = refrigeracion
         self.fechaCaducidad = fechaCaducidad
+
+    # Metodos Accesores
+    def getPrecio(self):
+        return self.precio
+
+    def getCantidad(self):
+        return self.cantidad
+
+    def getMarca(self):
+        return self.marca
+
+    def getPresentacion(self):
+        return self.presentacion
+
+    def getRefrigeracion(self):
+        return self.refrigeracion
+
+    def getFechaCaducidad(self):
+        return self.fechaCaducidad
+
+    def __str__(self):
+        return f'Identificador : {self.identificador}.\nNombre: {self.nombre}\nFechaPreparacio: {self.fechaPreparacion}\
+                \nPrecio: {self.precio}\nCantidad: {self.cantidad}\nMarca: {self.marca}\nPresentacion: {self.presentacion}\
+                \nRefrigeracion: {self.refrigeracion}\nFechaCaducidad: {self.fechaCaducidad}'
+
+    def __iter__(self):
+        return iter([self.identtificador, self.nombre, self.fechaPreparacion, self.precio, self.cantidad, self.marca, self.presentacion, self.refrigeracion, self.fechaCaducidad])
--- src/classes/Sucursales.py
+++ src/classes/Sucursales.py
@@ -1,27 +1,29 @@
 import Interfaz
 import csv
+
+
 class Sucursal(Interfaz.Interfaz):
-    
-    def __init__(self,identificador, nombre,fechaDeApertura, direccion, telefonos):
-        super().__init__(identificador, nombre,fechaDeApertura,direccion, telefonos)
+
+    def __init__(self, identificador, nombre, fechaDeApertura, direccion, telefonos):
+        super().__init__(identificador, nombre, fechaDeApertura, direccion, telefonos)
         self.numEmpleados = 0
 
     def numeroEmpleados(self):
         """ Realiza la lectura del archivo que contiene los empleados de la sucursal.
         """
-        with open('../assets/Empleados.csv', mode = 'r') as file:
-            reader = csv.reader(file)  
+        with open('../assets/Empleados.csv', mode='r') as file:
+            reader = csv.reader(file)
             empleados = 0
             for rows in reader:
                 print(type(rows[7]))
                 if self.getIdentificador() == rows[7]:
-                    empleados += 1     
-            self.numEmpleados=empleados
-           
+                    empleados += 1
+            self.numEmpleados = empleados
+
     def getNumEmpleados(self):
         self.numeroEmpleados()
-        return self.numEmpleados 
-    
+        return self.numEmpleados
+
     def __str__(self):
         numeros = ''
         empleados = self.getNumEmpleados()
@@ -34,4 +36,4 @@ class Sucursal(Interfaz.Interfaz):
 #     G = Sucursal('S1', 'PP',1,'Peña',[4444])
 #     print(G)
 
-# main()    
+# main()
