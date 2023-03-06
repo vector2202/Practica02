2df36dcd0982457ec5ee1ba95d2ff0735248fed7
Fernanda <ferame.om@gmail.com>
2023-03-06 00:07:24 -0600

    Menu

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Practica02
+# Practica02 Sistema de Archivos VS Bases de datos
+## Integrantes
+
+Osorio Morales Fernanda Ameyalli 
+Víctor Federico Torres Trejo 
+Ulises Josué Anaya Pérez
+Diego Castro Rendón
+José Luis López Bautista
---
+++ SRC/EGA.py
@@ -0,0 +1,36 @@
+from SRC import Main
+def consultarEmpleado():
+    print('estas consultando')
+
+def consultarProducto():
+    print('estas consultando')
+
+def consultarSucursal():
+    print('estas consultando')
+
+def agregarEmpleado():
+    print('estas agregando')
+
+def agregarProducto():
+    print('estas agregando')
+
+def agregarSucursal():
+    print('estas agregando')
+
+def eliminarEmpleado():
+    print('estas eliminando')
+
+def eliminarProducto():
+    print('estas eliminando')
+
+def eliminarSucursal():
+    print('estas eliminando')
+
+def editarEmpleado():
+    print('estas eliminando')
+
+def editarProducto():
+    print('estas eliminando')
+
+def editarSucursal():
+    print('estas eliminando')
