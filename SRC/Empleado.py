929561ca32d55d103f2e1862b24449e5de1f9759
ulisess_ap <ulisessap12@ciencias.unam.mx>
2023-03-05 20:00:04 -0600

    clase empleado y clase producto

---
+++ SRC/Empleado.py
@@ -0,0 +1,12 @@
+class Empleado:
+
+    def __init__(self, idEmpleado, nombre, direccion, telefono, fechaNacimiento, correo, puesto, sucursalTrabajo):
+
+        self.idEmpleado = idEmpleado
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.fechaNacimiento = fechaNacimiento
+        self.correo = correo
+        self.puesto = puesto
+        self.sucursalTrabajo = sucursalTrabajo
---
+++ SRC/Producto.py
@@ -0,0 +1,13 @@
+class Producto:
+
+    def __init__(self, idProducto, precio, nombre, cantidad, marca, presentacion, refrigeracion, fechaPreparacion, fechaCaducidad):
+
+        self.idProducto = idProducto
+        self.precio = precio
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.marca = marca
+        self.presentacion = presentacion
+        self.refrigeracion = refrigeracion
+        self.fechaPreparacion = fechaPreparacion
+        self.fechaCaducidad = fechaCaducidad
