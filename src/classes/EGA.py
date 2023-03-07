fd2ad378d45ecd37e802413281a6e730ae87818a
DiegoCastroRendon <diego.castro.rendon@ciencias.unam.mx>
2023-03-06 20:37:03 -0500

    Documentacion de EGA

--- src/classes/EGA.py
+++ src/classes/EGA.py
@@ -8,6 +8,7 @@ from Empleado import Empleado
 from Producto import Producto
 
 def agregarEmpleado():
+    '''Funcion que permite llenar los datos del empleado'''
     ID = str((Manejador.obtenerTamano('Empleado'))+1)
     print('El ID del nuevo empleado es ' + ID)
     nombre = input('Ingresa el nombre del empleado: ')
@@ -22,6 +23,7 @@ def agregarEmpleado():
 
 
 def agregarProducto():
+    '''Funcion que permite llenar los datos del producto'''
     ID = str((Manejador.obtenerTamano('Producto'))+1)
     print('El ID del nuevo producto es ' + ID)
     nombre = input('Ingresa el nombre del producto: ')
@@ -37,6 +39,7 @@ def agregarProducto():
    
 
 def agregarSucursal():
+    '''Funcion que permite llenar los datos de la sucursal'''
     id = str(Manejador.obtenerTamano('Sucursal')+1)
     print('El ID de la nueva sucursal es: ' + id)
     nombre = input('Ingresa el nombre de la sucursal: ')
@@ -47,37 +50,44 @@ def agregarSucursal():
     Manejador.agregar(SucursalNueva,'Sucursal')
 
 def consultarEmpleado():
+    '''Funcion que permite consular al objeto empleado'''
     IDaConsultar = input('Ingrese el ID del empleado que desea consultar')
     empleadoGenerico = Empleado("", "", "", "", "", "", "", "")
     Manejador.consultar(IDaConsultar,empleadoGenerico,'Empleado')
 
 def consultarProducto():
+    '''Funcion que permite consular al objeto Producto'''
     IDaConsultar = input('Ingrese el ID del producto a consultar')
     productoGenerico = Producto("", "", "", "", "", "", "", "", "")
     Manejador.consultar(IDaConsultar, productoGenerico, 'Producto')
 
 def consultarSucursal():
+    '''Funcion que permite consular al objeto Sucursal'''
     IDaConsultar = input('Ingrese el ID de la sucursal a consultar')
     sucursalGenerico = Sucursal("", "", "", "", "")
     Manejador.consultar(IDaConsultar, sucursalGenerico, 'Sucursal')
 
 
 def eliminarEmpleado():
+    '''Funcion que elimina el dato de empleado deseado'''
     IDaEliminar = input('Ingrese el ID del empleado que desea eliminar')
     empleadoGenerico = Empleado("", "", "", "", "", "", "", "")
     Manejador.eliminar(IDaEliminar,empleadoGenerico,'Empleado')
 
 def eliminarProducto():
+    '''Funcion que elimina el dato de producto deseado'''
     IDaEliminar = input('Ingrese el ID del producto que desea eliminar')
     productoGenerico = Producto("", "", "", "", "", "", "", "", "")
     Manejador.eliminar(IDaEliminar, productoGenerico, 'Producto')
 
 def eliminarSucursal():
+    '''Funcion que elimina el dato de sucursal deseado'''
     IDaEliminar = input('Ingrese el ID de la sucursal que desea eliminar')
     sucursalGenerico = Sucursal("", "", "", "", "")
     Manejador.eliminar(IDaEliminar, sucursalGenerico, 'Sucursal')
 
 def editarEmpleado():
+    '''Funcion que perimte editar datos del empleado'''
     IDaEditar = input('Ingrese el ID del empleado que desea editar: ')
     nombre = input('Ingresa el nombre del empleado: ')
     nacimiento = input('Ingresa su fecha de nacimiento: ')
@@ -90,6 +100,7 @@ def editarEmpleado():
     Manejador.editar(IDaEditar, EmpleadoEditado,'Empleado')
 
 def editarProducto():
+    '''Funcion que perimte editar datos del producto'''
     IDaEditar = input('Ingrese el ID del producto que desea editar: ')
     nombre = input('Ingresa el nombre del producto: ')
     fechaPreparacion = input('Ingresa la fecha de preparación del producto: ')
@@ -104,6 +115,7 @@ def editarProducto():
 
 
 def editarSucursal():
+    '''Funcion que perimte editar datos de la sucursal'''
     IDaEditar = input('Ingrese el ID de la sucursal que desea editar: ')
     nombre = input('Ingresa el nombre de la sucursal: ')
     fechaApertura = input('Ingresa la fecha de Apertura: ')
